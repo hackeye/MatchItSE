b4c85a1b364f977834dc6b75d684c6d94f8da6f4
Thorsten Henke <mail@thorsten-henke.de>
2016-09-14 14:58:52 +0200

    Added namespace for imported functions

--- R/Lechner-SE.R
+++ R/Lechner-SE.R
@@ -13,13 +13,14 @@
 #' att(obj = m.out, Y = lalonde$re78)
 #' lechner_se(obj = m.out, Y = lalonde$re78)
 lechner_se <- function(obj, Y){
+  stopifnot(methods::is(obj, "matchit"))
   # Lechner 2001
   ww <- obj$weights
   tt <- obj$treat
   ct <- sum(ww > 0 & tt == 1)  # Count treated
   cc <- sum(ww > 0 & tt == 0)  # Count control
   kk <- (ct/cc) * ww[ww > 0 & tt == 0] # How often "used" as a match?
-  vart <- var(Y[ww > 0 & tt == 1])  # raw variance treated
-  varc <- var(Y[ww > 0 & tt == 0])  # raw variance control
+  vart <- stats::var(Y[ww > 0 & tt == 1])  # raw variance treated
+  varc <- stats::var(Y[ww > 0 & tt == 0])  # raw variance control
   sqrt( vart/ct + (sum(kk^2)*varc)/ct^2 )  # Lechner's formula
 }
