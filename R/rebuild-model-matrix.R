69174303a05acbedd33b92d6d585cf7ccb38e97c
Thorsten Henke <mail@thorsten-henke.de>
2016-09-14 14:59:48 +0200

    Added warning message for user

--- R/rebuild-model-matrix.R
+++ R/rebuild-model-matrix.R
@@ -1,10 +1,21 @@
-brunhild <- function(fit){
-	# Rekonstruiert die Modelmatrix
-  # Nur bei Fullmatching!
-  # model[[1]]$call$method == "full"
+#' @examples
+#' data('lalonde')
+#' m.out  <- matchit(treat ~ educ + black, data = lalonde, method = 'full')
+#' att(obj = m.out, Y = lalonde$re78)
+#' abadie_imbens_se(m.out, lalonde$re78)  # FAILS!
+#' m.out <- add_model_matrix(m.out)
+#' abadie_imbens_se(m.out, lalonde$re78)
+#' @export
+add_model_matrix <- function(fit){
+  warning("\nThis function is experimental!
+Most methods for estimating standard errors are only
+documented for NN matching.\nUse with caution!")
+  if (is.null(fit$call$method) || fit$call$method != "full") {
+    stop("This function only works for Full Matching")
+  }
 	p <- max(table(fit$subclass))
 	n <- sum(fit$treat)
-	mm <- matrix(NA, nr=n, nc=p)
+	mm <- matrix(NA, nrow = n, ncol = p)
 	ts <- fit$subclass[fit$treat == 1]
 	cs <- fit$subclass[fit$weights > 0 & fit$treat == 0]
 	for (i in 1:n){
@@ -14,5 +25,6 @@ brunhild <- function(fit){
 		}
 	}
 	rownames(mm) <- names(ts)
-	return(mm)
+	fit$match.matrix <- mm
+	return(fit)
 }
