cd75b82619c907c83242cc790abf7b8f82ab65d7
Thorsten Henke <mail@thorsten-henke.de>
2016-09-14 15:00:14 +0200

    removed already imported namespace in return

--- src/ffastlm.cpp
+++ src/ffastlm.cpp
@@ -5,18 +5,16 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 List fastReg(NumericVector y, NumericMatrix x, NumericVector w){
-	int n = x.nrow(), k = x.ncol(); 
-	arma::mat X(x.begin(), n, k, false); 
-	arma::colvec Y(y.begin(), n, false); 
-	arma::colvec W = arma::sqrt(arma::vec(w.begin(), w.length(), false)); 
-	X = arma::diagmat(W)*X; 
-	Y %= W; 
-	
-	int df = n - k; 
-	arma::colvec coef = arma::solve(X, Y); 
-	arma::colvec res = Y - X*coef; 
-	double s2 = std::inner_product(res.begin(), res.end(), res.begin(), 0.0)/df; 
-	arma::mat vcov = s2*arma::pinv(X.t()*X); 
-
-	return Rcpp::List::create(Named("coef")=coef, Named("vcov")=vcov); 
+	int n = x.nrow(), k = x.ncol();
+	arma::mat X(x.begin(), n, k, false);
+	arma::colvec Y(y.begin(), n, false);
+	arma::colvec W = arma::sqrt(arma::vec(w.begin(), w.length(), false));
+	X = arma::diagmat(W)*X;
+	Y %= W;
+	int df = n - k;
+	arma::colvec coef = arma::solve(X, Y);
+	arma::colvec res = Y - X*coef;
+	double s2 = std::inner_product(res.begin(), res.end(), res.begin(), 0.0)/df;
+	arma::mat vcov = s2*arma::pinv(X.t()*X);
+	return List::create(Named("coef")=coef, Named("vcov")=vcov);
 }
