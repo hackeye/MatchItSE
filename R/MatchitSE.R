f3c931a91b4eead95cc25cbc12072282fdd238b6
Thorsten Henke <mail@thorsten-henke.de>
2016-09-27 20:07:08 +0200

    Added RcppImports and slightly verbose package description

---
+++ R/MatchitSE.R
@@ -0,0 +1,13 @@
+#' MatchItSE: methods for Standard Error estimation
+#'
+#' Contains auxiliary functions for calculating Standard Errors for
+#' matched samples from the MatchIt package. Please note, that Matchit
+#' does not estimate Standard Errors by intention, since matching is seen by the
+#' package’s authors as a data preprocessing step.
+#'
+#' @docType package
+#' @name MatchItSE
+#'
+#' @importFrom Rcpp evalCpp
+#' @useDynLib MatchItSE
+NULL
